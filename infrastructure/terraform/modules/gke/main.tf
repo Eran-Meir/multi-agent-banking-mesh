2c985ef907ff16ed599c6a11846f365a8a614b84
Eran Meir <Eran Meir>
2026-06-14 04:47:42 +0300

    fix: Disable GKE deletion_protection to allow automated teardown

--- infrastructure/terraform/modules/gke/main.tf
+++ infrastructure/terraform/modules/gke/main.tf
@@ -1,8 +1,11 @@
-﻿resource "google_container_cluster" "primary" {
+resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
   network  = var.network_name
   subnetwork = var.subnet_name
+
+  # Allow automated killswitch teardown
+  deletion_protection = false
 
   # Enable Autopilot
   enable_autopilot = true
